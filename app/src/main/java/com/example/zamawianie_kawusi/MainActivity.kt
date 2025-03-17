4735e564d026af81085f968d30d049a02792a10f
olke <jajo@bajo.pl>
2025-03-17 11:27:02 +0100

    dodanie dzialania programu

--- app/src/main/java/com/example/zamawianie_kawusi/MainActivity.kt
+++ app/src/main/java/com/example/zamawianie_kawusi/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.zamawianie_kawusi
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,46 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+            val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
+            val imageView: ImageView = findViewById(R.id.imageview)
+            val radioEspresso = findViewById<RadioButton>(R.id.radioespresso)
+            val radioLatte = findViewById<RadioButton>(R.id.radiolatte)
+            val radioCapuccino = findViewById<RadioButton>(R.id.radiocappuccino)
+
+            radioEspresso.setOnCheckedChangeListener { _, isChecked ->
+                imageView.setImageResource(R.drawable.espresso)
+            }
+
+            radioLatte.setOnCheckedChangeListener { _, isChecked ->
+                imageView.setImageResource(R.drawable.latte)
+            }
+
+        radioCapuccino.setOnCheckedChangeListener { _, isChecked ->
+            imageView.setImageResource(R.drawable.capuccino)
+        }
+
+
+            val myTextView: TextView = findViewById(R.id.textview2)
+            val myseekBar = findViewById<SeekBar>(R.id.seekBar)
+            myseekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                    myTextView.text = "wartosc suwaka: $p1"
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+
+                }
+
+
+
+
+        })
     }
 }
+
+
